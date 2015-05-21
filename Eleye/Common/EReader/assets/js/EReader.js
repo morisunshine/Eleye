1105bfc260c495732bd63e864b60ce50ecd38cb7
jerryni <jerryni2014@gmail.com>
2015-05-21 22:55:33 +0800

    delete the nosense code

--- Eleye/Common/EReader/assets/js/EReader.js
+++ Eleye/Common/EReader/assets/js/EReader.js
@@ -91,19 +91,6 @@
           iframe.parentNode.removeChild(iframe);
           iframe = null;    
         }
-
-        // console = new Object();
-        // console.log = function(log) {
-        //   var iframe = document.createElement("IFRAME");
-        //   iframe.setAttribute("src", "ios-log:#iOS#" + log);
-        //   document.documentElement.appendChild(iframe);
-        //   iframe.parentNode.removeChild(iframe);
-        //   iframe = null;    
-        // };
-        // console.debug = console.log;
-        // console.info = console.log;
-        // console.warn = console.log;
-        // console.error = console.log;
     };
 
     window.EReader = EReader;
